5ea9cb11d3e9d7362fadfabb489fc6a9623f7cb3
yosiknorman <yosikbrebes@gmail.com>
2019-07-07 21:53:44 +0800

    baru Min Jul  7 21:53:44 WITA 2019

---
+++ BOT.R
@@ -0,0 +1,17 @@
+BOT = 
+  "Weather Warning Update PHE ONWJ EAST 15 May 2019 14:30 LT
+Please be aware
+
+Potential of MODERATE to HEAVY rainfall over Seribu Island Waters and Western Java Sea include PHE ONWJ EAST offshore area.
+
+This condition is predicted to continue untill 16.30 LT
+
+24hrs duty forecaster
+
+Marine Weather Services
+------------------------------------------------------
+BADAN METEOROLOGI KLIMATOLOGI dan GEOFISIKA (BMKG)
+Jl. Angkasa I No. 2 Kemayoran
+10720 JAKARTA
+T: +62 815 1927 3737
+E: layanan.maritim@bmkg.go.id"
